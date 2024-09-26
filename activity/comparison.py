14816c513c1e12760761c515a80d604c0902364a
Lula <mlusan95@gmail.com>
2024-09-26 16:36:49 -0400

    Student class created

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,7 @@
 # add your get_student_with_more_classes function here!
+
+def get_student_with_more_classes(student_a, student_b):
+    if student_a.get_num_classes() >= student_b.get_num_classes():
+        return student_a.name
+    else:
+        return student_b.name
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,15 @@
 # add your Student class here!
+class Student:
+    def __init__(self, name, year, classes):
+        self.name = name
+        self.year = year
+        self.classes = classes
+
+    def add_class(self, class_name):
+        self.classes.append(class_name)
+
+    def get_num_classes(self):
+        return len(self.classes)
+    
+    def summary(self):
+        return f'{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes'
